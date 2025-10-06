484d8556a6647d92107239b1175e740da091fc05
PatriciaOM23 <patricia.olmedo.majano23@gmail.com>
2025-10-06 13:47:24 +0200

    Create Main.java
    Ejercicios para entender los atributos y métodos, tanto públicos como privados.

---
+++ src/Main.java
@@ -0,0 +1,72 @@
+import utilidades.CuentaBancaria;
+
+public class Main {
+    public static void main(String[] args) {
+        CuentaBancaria cuenta = new CuentaBancaria();
+
+        // ================================
+        // EJERCICIO 1: Uso de atributos
+        // ================================
+        // 1. Intenta acceder directamente al atributo saldo:
+        //    System.out.println(cuenta.saldo);
+        // 2. Observa el error de compilación.
+
+        // Tu código aquí ↓
+
+       // System.out.println(cuenta.saldo());
+
+        // ================================
+        // EJERCICIO 2: Métodos públicos
+        // ================================
+        // 1. Ingresa 100€ usando depositar().
+        // 2. Retira 30€ usando retirar().
+        // 3. Imprime el saldo con getSaldo().
+
+        // Tu código aquí ↓
+            
+       /*     cuenta.depositar(100);
+            cuenta.retirar(30);
+            cuenta.getSaldo();
+
+             */
+        // ================================
+        // EJERCICIO 3: Métodos privados
+        // ================================
+        // 1. Intenta llamar al método registrarOperacion("Hackeo", 9999).
+        // 2. Observa el error de compilación.
+
+        // Tu código aquí ↓
+        // cuenta.registrarOperacion("Hackeo", 2000);
+        // ================================
+        // EJERCICIO 4: Libre
+        // ================================
+        // Puedes hacer pruebas adicionales con los métodos disponibles
+        // para asegurarte de que comprendes la diferencia entre public y private.
+        cuenta.depositar(100);
+        cuenta.depositar(20);
+        //No funciona cuenta.getSaldo a solas porque es una operacion para que vea el usuario no para nosotros mismos
+        System.out.println(cuenta.getSaldo());
+       
+
+    }
+}
+
+// ================================
+// RESPUESTAS DE REFLEXIÓN
+// ================================
+// Ejercicio 1: ¿por qué no puedo acceder al saldo directamente?
+// Respuesta: Porque es una clase privada
+
+// Ejercicio 2: ¿por qué sí puedo usar los métodos depositar(), retirar() y getSaldo()?
+// Respuesta: Porque son clases públicas
+
+// Ejercicio 3: ¿qué significa el error al intentar llamar a registrarOperacion()?
+// Respuesta: Que "no existe" ya que es una clase privada entonces para el usuario "no existe" y por ese no se le puede llamar.
+
+// Ejercicio 4: 
+// - ¿Qué ventajas tiene que saldo sea private?
+// Que no cualquiera puede acceder al saldo ajeno
+// - ¿Qué pasaría si saldo fuera public?
+// Que todos podrían acceder a mi saldo
+// - ¿Por qué registrarOperacion es private en lugar de public?
+// Respuesta: Porque quiero que sea una operación interna que no se muestre al usuario 
